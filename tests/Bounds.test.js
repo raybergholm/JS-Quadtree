42a40b38cac27185ae094cf9f4ea95e95b71e396
Ray Bergholm <raybergholm@gmail.com>
2018-03-09 13:51:43 +0200

    increase Bounds and Divider test granularity

--- tests/Bounds.test.js
+++ tests/Bounds.test.js
@@ -16,68 +16,93 @@ const boundsChecker = (reference, expected) => reference.x === expected.x &&
     reference.height === expected.height;
 
 describe("Bounds class testing", () => {
-    it("No parameters result in zeroed bounds", () => {
-        const zeroed = new Bounds();
-        expect(boundsChecker(zeroed, ZeroBounds)).to.be.true;
-    });
-
-    it("Passing parameters should not result in zeroed bounds", () => {
-        const area = new Bounds({
-            x: 50,
-            y: 50,
-            width: 100,
-            height: 100
+    describe("Normal use cases", () => {
+        it("No parameters result in zeroed bounds", () => {
+            const zeroed = new Bounds();
+            expect(boundsChecker(zeroed, ZeroBounds)).to.be.true;
         });
-        expect(boundsChecker(area, ZeroBounds)).to.be.false;
-    });
-
-    it("Full parameters should match", () => {
-        const area = new Bounds({
-            x: 50,
-            y: 50,
-            width: 100,
-            height: 100
+    
+        it("Passing parameters should not result in zeroed bounds", () => {
+            const area = new Bounds({
+                x: 50,
+                y: 50,
+                width: 100,
+                height: 100
+            });
+            expect(boundsChecker(area, ZeroBounds)).to.be.false;
         });
-        expect(boundsChecker(area, {
-            x: 50,
-            y: 50,
-            width: 100,
-            height: 100
-        })).to.be.true;
-    });
-
-    it("Point bounds should have x & y but zero width and height", () => {
-        const area = new Bounds({
-            x: 25,
-            y: 25
+    
+        it("Full parameters should match", () => {
+            const area = new Bounds({
+                x: 50,
+                y: 50,
+                width: 100,
+                height: 100
+            });
+            expect(boundsChecker(area, {
+                x: 50,
+                y: 50,
+                width: 100,
+                height: 100
+            })).to.be.true;
         });
-        expect(boundsChecker(area, {
-            x: 25,
-            y: 25,
-            width: 0,
-            height: 0
-        })).to.be.true;
-    });
-
-    it("Bounds with no x & y starts at origin", () => {
-        const area = new Bounds({
-            width: 12.12,
-            height: 101.5
+    
+        it("Point bounds should have x & y but zero width and height", () => {
+            const area = new Bounds({
+                x: 25,
+                y: 25
+            });
+            expect(boundsChecker(area, {
+                x: 25,
+                y: 25,
+                width: 0,
+                height: 0
+            })).to.be.true;
         });
-        expect(boundsChecker(area, {
-            x: 0,
-            y: 0,
-            width: 12.12,
-            height: 101.5
-        })).to.be.true;
+    
+        it("Bounds with no x & y starts at origin", () => {
+            const area = new Bounds({
+                width: 12.12,
+                height: 101.5
+            });
+            expect(boundsChecker(area, {
+                x: 0,
+                y: 0,
+                width: 12.12,
+                height: 101.5
+            })).to.be.true;
+        });
     });
 
-    it("Invalid input should not be accepted", () => {
-        expect(() => new Bounds({
-            x: "hello",
-            y: "world",
-            width: "no",
-            height: "1234aaa"
-        })).to.throw(Error);
+    describe("Invalid use cases", () => {
+        it("Strings should throw", () => {
+            expect(() => new Bounds({
+                x: "this is bad input"
+            })).to.throw();
+        });
+
+        it("String emojis should throw too", () => {
+            expect(() => new Bounds({
+                x: "😲"
+            })).to.throw();
+        });
+
+        it("Arrays should throw", () => {
+            expect(() => new Bounds({
+                x: []
+            })).to.throw();
+        });
+
+        it("Objects should throw", () => {
+            expect(() => new Bounds({
+                x: {x: 42, y: 42}
+            })).to.throw();
+        });
+
+        it("Functions should throw", () => {
+            expect(() => new Bounds({
+                x: () => 42
+            })).to.throw();
+        });
     });
 });
--- tests/Divider.test.js
+++ tests/Divider.test.js
@@ -14,38 +14,96 @@ const dividerChecker = (reference, expected) => reference.x === expected.x &&
     reference.y === expected.y;
 
 describe("Dividers class testing", () => {
-    it("No parameters result in defaults", () => {
-        const divider = new Divider();
-        expect(dividerChecker(divider, DefaultDivider)).to.be.true;
-    });
-
-    it("Passing parameters should not result in defaults", () => {
-        const divider = new Divider({
-            x: 30,
-            y: 25
+    describe("Normal use cases", () => {
+        it("No parameters result in defaults", () => {
+            const divider = new Divider();
+            expect(dividerChecker(divider, DefaultDivider)).to.be.true;
         });
-        expect(dividerChecker(divider, DefaultDivider)).to.be.false;
+
+        it("Passing parameters should not result in defaults", () => {
+            const divider = new Divider({
+                x: 30,
+                y: 25
+            });
+            expect(dividerChecker(divider, DefaultDivider)).to.be.false;
+        });
+
+        it("Partial parameters should be ok", () => {
+            expect(dividerChecker(new Divider({
+                x: 30
+            }), {
+                x: 30,
+                y: 0
+            })).to.be.true;
+
+            expect(dividerChecker(new Divider({
+                y: 25
+            }), {
+                x: 0,
+                y: 25
+            })).to.be.true;
+        });
     });
 
-    it("Passing invalid input should throw errors", () => {
-        expect(() => new Divider({
-            x: -25,
-            y: 2000
-        })).to.throw();
+    describe("Invalid use cases", () => {
+        it("Negative input should throw", () => {
+            expect(() => new Divider({
+                x: -25,
+                y: 50
+            })).to.throw();
 
-        expect(() => new Divider({
-            x: 50,
-            y: "this is bad input" 
-        })).to.throw();
+            expect(() => new Divider({
+                x: -1
+            })).to.throw();
 
-        expect(() => new Divider({
-            x: () => 10,
-            y: []
-        })).to.throw();
+            expect(() => new Divider({
+                y: -100
+            })).to.throw();
+        });
 
-        expect(() => new Divider({
-            x: 10,
-            y: "😲"
-        })).to.throw();
+        it("Input >100 should throw", () => {
+            expect(() => new Divider({
+                x: 1000,
+                y: 50
+            })).to.throw();
+
+            expect(() => new Divider({
+                x: 100.1
+            })).to.throw();
+
+            expect(() => new Divider({
+                y: 101
+            })).to.throw();
+        });
+
+        it("Strings should throw", () => {
+            expect(() => new Divider({
+                y: "this is bad input"
+            })).to.throw();
+        });
+
+        it("String emojis should throw too", () => {
+            expect(() => new Divider({
+                y: "😲"
+            })).to.throw();
+        });
+
+        it("Arrays should throw", () => {
+            expect(() => new Divider({
+                y: []
+            })).to.throw();
+        });
+
+        it("Objects should throw", () => {
+            expect(() => new Divider({
+                y: {x: 42, y: 42}
+            })).to.throw();
+        });
+
+        it("Functions should throw", () => {
+            expect(() => new Divider({
+                y: () => 42
+            })).to.throw();
+        });
     });
 });
